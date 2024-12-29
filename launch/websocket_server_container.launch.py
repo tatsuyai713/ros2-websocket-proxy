280a8bdd44cc86c1f96e20fe29bd57a4da685419
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-12-29 15:56:30 +0900

    Add file for container

---
+++ launch/websocket_server_container.launch.py
@@ -0,0 +1,21 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('yaml_file', default_value='/ros2_app/config/server_topics.yaml', description='Path to the YAML file'),
+        DeclareLaunchArgument('port', default_value='9091', description='WebSocket server port'),
+        Node(
+            package='ros2_websocket_proxy',
+            executable='generic_server',
+            name='generic_server',
+            output='screen',
+            parameters=[{
+                'yaml_file': LaunchConfiguration('yaml_file'),
+                'port': LaunchConfiguration('port'),
+            }],
+        ),
+    ])
